ad680ddb8c69d7872c06820d1b30597bf0a348e1
HW Computer <derex1987@gmail.com>
2023-04-17 19:29:56 -0500

    Woohoo progress

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-function parseCSV(csv) {
-    
+let className = '';
+const submitButton = document.getElementById('subBut');
+submitButton.addEventListener("click", parseName);
+function parseName() {
+    className = document.getElementById('className').value;
+    console.log(className);
 }
