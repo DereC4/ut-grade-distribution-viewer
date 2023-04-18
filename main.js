14f4709910d71f2cbc2546a9757613877a4d2ddf
HW Computer <derex1987@gmail.com>
2023-04-18 14:24:37 -0500

    Papap

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-let className = '';
-let classNum = '';
-let department = '';
 const submitButton = document.getElementById('subBut');
 submitButton.addEventListener("click", parseName);
 function parseName() {
+    let className = '';
+    let classNum = '';
+    let department = '';
     className = document.getElementById('courseName').value;
     classNum = document.getElementById('courseNum').value;
     department = document.getElementById('courseField').value;
-    console.log(department, classNum, className);
     if(department.length > 3) {
         alert("Invalid Department");
         return;
+    } 
+    if(className == '' && classNum == '' && department == '') {
+        alert("At least fill out the form...");
+        return;
     }
+    if(className == '' || classNum == '' || department == '') {
+        alert("Missing something?");
+        return;
+    }
+    console.log(department, classNum, className);
+    searchData(department, classNum, className);
+}
+
+function searchData(department, num, name) {
+    
 }
