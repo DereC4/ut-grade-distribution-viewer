912d747114c1412a9708382161d0b0f23edaf746
HW Computer <derex1987@gmail.com>
2023-04-17 19:39:36 -0500

    More te;xt fields

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 let className = '';
+let classNum = '';
+let department = '';
 const submitButton = document.getElementById('subBut');
 submitButton.addEventListener("click", parseName);
 function parseName() {
     className = document.getElementById('className').value;
+    department = document.getElementById('').value;
+    department = document.getElementById('').value;
     console.log(className);
 }
