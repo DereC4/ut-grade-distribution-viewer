5b7a2b06bdb6bd5552fc24e80bedc4357eb4a2b0
Derek Chen <derex1987@gmail.com>
2024-04-20 16:16:57 -0500

    fix: chart.js bug not changing color scheme finally fixedddd

--- docs/colorScheme.js
+++ docs/colorScheme.js
@@ -3,4 +3,7 @@ colorToggle.addEventListener("change", () => {
   const darkModeEnabled = document.body.classList.toggle("dark");
   Chart.defaults.global.defaultFontColor = darkModeEnabled ? "#484b6a" : "#F8F0E5";
   // Chart.defaults.plugins.legend.labels.color = isDarkModeEnabled ? "#484b6a" : "#F8F0E5";
+  Chart.helpers.each(Chart.instances, (instance) => {
+    instance.chart.update();
+  });
 })
