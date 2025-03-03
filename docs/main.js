3d467352b12d927dddf62f4dbbe50980c31e83df
Derek Chen <derex1987@gmail.com>
2025-03-03 04:44:01 -0600

    feat: update database, update changelog blog

--- docs/main.js
+++ docs/main.js
@@ -67,6 +67,7 @@ async function parseName() {
  */
 async function PapaParse(department, num, name, instructor, sem) {
     const semesterURLs = {
+        'f2024': 'https://derec4.github.io/ut-grade-data/2024%20Fall.json',
         'sp2024': 'https://derec4.github.io/ut-grade-data/2024%20Spring.json',
         'f2023': 'https://derec4.github.io/ut-grade-data/2023%20Fall.json',
         's2023': 'https://derec4.github.io/ut-grade-data/2023%20Summer.json',
